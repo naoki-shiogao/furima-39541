f16b334c336d521d9c3a44f023d6da401ab2b45f
naoki-shiogao <hors1359@gmail.com>
2023-09-04 20:21:31 +0900

    ユーザー新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
 
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
-    validates [:last_name, :first_name], format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     # カタカナのみ許可する
-    validates [:last_name_reading, :first_name_reading], format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_reading, :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
   end
   validates :nickname, presence: true
   validates :nickname, presence: true
