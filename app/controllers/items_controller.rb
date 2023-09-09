4e35f671882d45e567f88df1ec615afa6311061b
naoki-shiogao <hors1359@gmail.com>
2023-09-09 16:40:35 +0900

    itemコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,11 +27,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
     else
-      render "edit"
+      render "edit", status: :unprocessable_entity
     end
   end
 
