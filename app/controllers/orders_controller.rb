af3499cdab45f931c55bb5162f89cdacd956d987
naoki-shiogao <hors1359@gmail.com>
2023-09-11 17:40:51 +0900

    出品者自身が購入画面に遷移しようとするとトップページに戻される

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,19 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :current_user?
 
   def index
     @item = Item.find(params[:item_id])
+  end
+
+
+
+  private
+
+  def current_user?
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to "/"
+    end
   end
 end
