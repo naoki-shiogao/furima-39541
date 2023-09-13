4ba2d623ec8ab18daa833b583010ccd2030f545f
naoki-shiogao <hors1359@gmail.com>
2023-09-13 15:09:06 +0900

    address,orderモデルの整理

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,5 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :order
   belongs_to :prefecture
-
-  with_options presence: true do
-    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :telephone_number, format: { with: /\d{10,11}/ }
-  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,20 @@
 class Order < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number
+
   belongs_to :user
   belongs_to :item
   has_one :address
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :city
+    validates :house_number
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+  end
+
+
 end
